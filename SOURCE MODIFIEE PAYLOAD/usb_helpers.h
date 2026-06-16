4570b18f5dd52474a42f31957ab156454842a681
Fsociety <180300849+NikoBellikJR31@users.noreply.github.com>
2026-06-16 17:05:57 +0200

    Delete SOURCE MODIFIEE PAYLOAD directory

--- SOURCE MODIFIEE PAYLOAD/usb_helpers.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <stdint.h>
-#include <dev/usb/usb.h>
-#include <dev/usb/usb_ioctl.h>
-
-int  usb_send_out(int fd, struct usb_fs_endpoint *ep,
-                  const uint8_t *data, uint32_t len, const char *tag);
-int  usb_send_cmd(int fd, struct usb_fs_endpoint *ep, uint8_t a, uint8_t b);
